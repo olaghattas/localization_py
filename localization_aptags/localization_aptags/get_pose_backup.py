774230ce38fdf3d7c9b705fd906f883be6518a52
olaghattas <olaghattas@hotmail.com>
2023-07-19 04:06:37 -0400

    update yw to tf

--- localization_aptags/localization_aptags/get_pose_backup.py
+++ localization_aptags/localization_aptags/get_pose_backup.py
@@ -39,7 +39,7 @@ class GetPose(Node):
 
         self.tf_buffer = Buffer()
         self.tf_listener = TransformListener(self.tf_buffer, self)
-        self.used_apriltags = [0, 2, 13]  # add the apriltag ids that you used
+        self.used_apriltags = [0, 2, 4, 11, 13]  # add the apriltag ids that you used
         self.transform_aptag_in_cam_dict = {}  # location of apriltags in camera frame
         self.transform_aptag_in_world_dict = {}  # global location of apriltags
 
@@ -48,16 +48,35 @@ class GetPose(Node):
         # incase we decided to get this in another way
         self.t_cam_in_baselink = None
         self.t_opcam_in_cam = None
+        self.apriltag_to_world_convention = None
 
-    def yaw_to_quaternion(self, yaw):
-        half_yaw = yaw * 0.5
-        cos_half_yaw = np.cos(half_yaw)
-        sin_half_yaw = np.sin(half_yaw)
+    # transformation between realsense and base_link
+    def publish_static_transform(self):
+        # optical frame follows the following Camera Optical Coordinate System: (X: Right, Y: Down, Z: Forward)
+        # Define the rotation angles
+        roll = math.pi
+        pitch = math.pi
+        yaw = math.pi / 2.0
 
-        quaternion = np.array([cos_half_yaw, 0, 0, sin_half_yaw])
-        return quaternion
+        # Create the Quaternion object
+        quaternion = Quaternion(axis=[0, 0, 1], radians=yaw) * Quaternion(axis=[0, 1, 0], radians=pitch) * Quaternion(
+            axis=[1, 0, 0], radians=roll)
 
+        static_transform = TransformStamped()
+        static_transform.header.stamp = self.get_clock().now().to_msg()
+        static_transform.header.frame_id = 'base_link'
+        static_transform.child_frame_id = 'camera_color_optical_frame'
 
+        static_transform.transform.translation.x = 0.0  # Set translation values
+        static_transform.transform.translation.y = 0.0
+        static_transform.transform.translation.z = 0.4  # meters
+
+        static_transform.transform.rotation.x = quaternion[0]  # Set rotation values
+        static_transform.transform.rotation.y = quaternion[1]
+        static_transform.transform.rotation.z = quaternion[2]
+        static_transform.transform.rotation.w = quaternion[3]
+
+        self.tf_broadcaster.sendTransform(static_transform)
 
     def publish_tf(self, robot_pose_aptags):
         static_transform = TransformStamped()
@@ -71,6 +90,47 @@ class GetPose(Node):
         static_transform.transform.rotation.w = robot_pose_aptags[2]
 
         self.tf_broadcaster.sendTransform(static_transform)
+
+    def fixed_transformations(self):
+        self.t_cam_in_baselink = np.array([
+            [1.0, 0.0, 0.0, 0.0],
+            [0.0, 0.0, 1.0, 0.0],
+            [0.0, -1.0, 0.0, 0.4],
+            [0.0, 0.0, 0.0, 1.0]
+        ])
+
+        self.t_opcam_in_cam = np.array([
+            [0.0, 0.0, 1.0, 0.0],
+            [-1.0, 0.0, 0.0, 0.0],
+            [0.0, -1.0, 0.0, 0.0],
+            [0.0, 0.0, 0.0, 1.0]
+        ])
+
+        # self.apriltag_to_world_convention = np.array([
+        #     [1.0, 0.0, 0.0, 0.0],
+        #     [0.0, 0.0, 1.0, 0.0],
+        #     [0.0, -1.0, 0.0, 0.4],
+        #     [0.0, 0.0, 0.0, 1.0]
+        # ])
+
+    def get_transform_matrix_aptags_from_json_file(self):
+        share_directory = get_package_share_directory('localization_aptags')
+        # Accessing a file in the package's share directory
+        file_path = os.path.join(share_directory, 'transforms.json')
+
+        f = open(file_path)
+        transforms = json.load(f)
+
+        for transform in transforms:
+            print(transform)
+            aptag_id = transform['id']
+            translation = tr.translation_matrix([transform['posX'], transform['posY'], transform['posZ']])
+            rotation = tr.quaternion_matrix(
+                [transform['oriX'], transform['oriY'], transform['oriZ'], transform['oriW']])
+            # Get the homogeneous transformation matrix
+            transform_aptag_in_world = np.dot(translation, rotation)
+            self.transform_aptag_in_world_dict[aptag_id] = transform_aptag_in_world
+        print(self.transform_aptag_in_world_dict)
 
     def get_transform_matrix_aptags_from_tf(self):
         for aptag in self.used_apriltags:
@@ -100,6 +160,39 @@ class GetPose(Node):
                 return
 
             print('self.transform_aptag_in_cam_dict', self.transform_aptag_in_cam_dict)
+
+    def get_transform_matrix_aptags_from_yaml_file(self):
+        share_directory = get_package_share_directory('localization_aptags')
+        # Accessing a file in the package's share directory
+        file_path = os.path.join(share_directory, 'transform_matrices_dictionary.yaml')
+
+        # Load YAML file
+        with open(file_path, 'r') as file:
+            yaml_data = yaml.safe_load(file)
+
+        # Initialize dictionary
+        transformations_dict = {}
+
+        # Extract transformations from YAML data
+        transformations = yaml_data['transformations']
+        for transformation in transformations:
+            # Extract ID and matrix
+
+            # to stop from using same apriltag id twice
+            matrix_id = transformation['id']
+            if matrix_id in self.transform_aptag_in_world_dict.keys():
+                self.get_logger().info("Apriltag with id " + str(matrix_id) + " already in use")
+                continue
+
+            matrix = transformation['matrix']
+            # Convert to numpy array
+            matrix_np = np.array(matrix)
+
+            # Store in dictionary
+            self.transform_aptag_in_world_dict[matrix_id] = matrix_np
+
+        # Print the dictionary
+        print("Transformations dictionary:\n", self.transform_aptag_in_world_dict)
 
     def apriltag_callback(self, msg):
         if msg.detections:
@@ -131,7 +224,6 @@ class GetPose(Node):
 
             print('self.transform_aptag_in_cam_dict', self.transform_aptag_in_cam_dict)
 
-
     def transform_cam_world_frame(self):
         robot_position = []
         transform_aptag_in_cam_dict = self.transform_aptag_in_cam_dict
@@ -139,8 +231,12 @@ class GetPose(Node):
             t_apriltag_to_world = self.transform_aptag_in_world_dict[aptag]
             t_apriltag_to_camera = transform_aptag_in_cam_dict[aptag]
 
+            # t_apriltag_to_world_convention =
+            # t_camera_to_robot = self.t_cam_in_baselink
+            # t_robot_in_world = np.dot(t_apriltag_to_world,
+            #                           np.dot(np.linalg.inv(t_apriltag_to_camera), np.linalg.inv(t_camera_to_robot)))
+
             t_robot_in_world = np.dot(t_apriltag_to_world, np.linalg.inv(t_apriltag_to_camera))
-            t_robot_in_world = np.dot(t_robot_in_world, np.linalg.inv(t_apriltag_to_camera))
 
             # Extract the robot coordinates and rotation from the transformation matrix
             robot_x = t_robot_in_world[0, 3]
